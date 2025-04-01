4217f4233af2bdc1d6ed0a3297fd6b6cee616f45
Timophey <ltimophey06@yandex.ru>
2025-04-02 01:10:48 +0300

    feat: Modal.ts (взято из ono-tebe-nado)

--- src/components/Modal.ts
+++ src/components/Modal.ts
@@ -1,18 +1,44 @@
+import { ensureAllElements, ensureElement } from "../utils/utils";
+import { View } from "./base/Component";
 import { IEvents } from "./base/events";
 
 
-export class Modal {
+interface IModalData {
+    content: HTMLElement;
+}
+
+export class Modal extends View<IModalData>{
     protected _closeButton: HTMLButtonElement;
     protected _content: HTMLElement;
 
-    constructor(container: HTMLElement, protected events: IEvents){}
+    constructor(container: HTMLElement, protected events: IEvents){
+        super(container, events);
+
+        this._closeButton = ensureElement<HTMLButtonElement>('modal__close', container);
+        this._content = ensureElement<HTMLElement>('.modal__content', container);
+
+        this._closeButton.addEventListener('click', this.close.bind(this));
+        this.container.addEventListener('click', this.close.bind(this));
+        this._content.addEventListener('click', (event) => event.stopPropagation());
+    }
 
     set content(value: HTMLElement){
         this._content.replaceChildren(value);
     }
-    open(){}
+    open() {
+        this.toggleClass(this.container, 'modal_active', true);
+        this.events.emit('modal:open');
+    }
 
-    close(){}
+    close() {
+        this.toggleClass(this.container, 'modal_active', false);
+        this.content = null;
+        this.events.emit('modal:close');
+    }
 
-    render(){}
+    render(data: IModalData): HTMLElement {
+        super.render(data);
+        this.open();
+        return this.container;
+    }
 }
