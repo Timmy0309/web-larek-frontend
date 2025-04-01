01b0a8a7d140845de70fd30ef069e5506f255d10
Timophey <ltimophey06@yandex.ru>
2025-04-01 23:01:49 +0300

    feat: OrderForm.ts

--- src/components/OrderForm.ts
+++ src/components/OrderForm.ts
@@ -6,16 +6,33 @@ import { ensureElement } from '../utils/utils';
 export class OrderForm extends Form<TOrderForm> {
 	protected _paymentCard: HTMLButtonElement;
 	protected _paymentCash: HTMLButtonElement;
-	protected _address: HTMLInputElement;
 
 	constructor(container: HTMLFormElement, events: IEvents) {
 		super(container, events);
+
+		this._paymentCard = ensureElement<HTMLButtonElement>('[name=card]', this.container);
+		this._paymentCash = ensureElement<HTMLButtonElement>('[name=cash]', this.container);
+
+		this._paymentCard.addEventListener('click', () => {
+			this._paymentCard.classList.add('button_alt_active');
+			this._paymentCard.classList.remove('button_alt');
+			this._paymentCash.classList.add('button_alt');
+			this._paymentCash.classList.remove('button_alt_active');
+			this.events.emit('order:change', { field: 'payment', value: 'online' });
+		});
+
+		this._paymentCash.addEventListener('click', () => {
+			this._paymentCash.classList.add('button_alt_active');
+			this._paymentCash.classList.remove('button_alt');
+			this._paymentCard.classList.add('button_alt');
+			this._paymentCard.classList.remove('button_alt_active');
+			this.events.emit('order:change', { field: 'payment', value: 'online' });
+		});
+
+
 	}
 
-	set payment(value: PaymentMethod) {
-		}
-
-	set address(value: string) {
-		this._address.value = value;
+	set address(val: string) {
+		(this.container.elements.namedItem('address') as HTMLSelectElement).value = val;
 	}
 }
