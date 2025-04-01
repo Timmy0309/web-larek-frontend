c566f8990cd37d65d076daeef3a869cdf68be3ef
Timophey <ltimophey06@yandex.ru>
2025-04-02 00:27:20 +0300

    feat: Basket.ts

--- src/components/Basket.ts
+++ src/components/Basket.ts
@@ -1,4 +1,5 @@
 import { cloneTemplate, createElement, ensureElement } from '../utils/utils';
+import { View } from './base/Component';
 import { IEvents } from './base/events';
 
 interface IBasketView {
@@ -7,18 +8,48 @@ interface IBasketView {
 	selected: string[];
 }
 
-export class Basket {
+export class Basket extends View<IBasketView>{
     static template = ensureElement<HTMLTemplateElement>('#basket');
 
 	protected _list: HTMLElement;
 	protected _total: HTMLElement;
 	protected _button: HTMLElement;
 
-	constructor(protected events: IEvents){}
+	constructor(protected events: IEvents){
+		super(cloneTemplate(Basket.template), events);
 
-    toggleButton(state: boolean){}
+		this._list = ensureElement<HTMLElement>('.basket__list', this.container);
+		this._button = this.container.querySelector('.basket__button');
+		this._total = this.container.querySelector('.basket__price');
 
-    set items(items: HTMLElement[]){}
+		if (this._button) {
+			this._button.addEventListener('click', () => {
+				this.events.emit('order:open');
+			});
+		}
 
-    set total(total: number){}
+		this.items = [];
+	}
+
+    toggleButton(state: boolean){
+		this.setDisabled(this._button, !state);
+	}
+
+    set items(items: HTMLElement[]){
+		if (items.length) {
+			this._list.replaceChildren(...items);
+			this.toggleButton(true);
+		} else {
+			this._list.replaceChildren(
+				createElement<HTMLParagraphElement>('p', {
+					textContent: 'Корзина пустая'
+				})
+			);
+			this.toggleButton(false);
+		}
+	}
+
+    set total(total: number){
+		this.setText(this._total, `${total} синапсов`);
+	}
 }
