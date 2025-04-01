1912b12a269bc4ea913db366d7ed3a478f7de4b0
Timophey <ltimophey06@yandex.ru>
2025-04-02 00:12:19 +0300

    feat: AppData.ts

--- src/components/AppData.ts
+++ src/components/AppData.ts
@@ -1,5 +1,6 @@
 import { IBasket, IProduct, PaymentMethod, TOrder } from '../types';
 import { IEvents } from './base/events';
+import { Model } from './base/Model';
 
 
 export class AppData {
@@ -15,16 +16,19 @@ export class AppData {
 		address: '',
 		payment: 'card',
 	};
+	loading: boolean;
 	formErrors: Partial<Record<keyof TOrder, string>> = {};
 
 	constructor(protected events: IEvents) {}
 
 	setItems(items: IProduct[]) {
-
+		this.items = items;
+		this.events.emit('items:change', this.items);
 	}
 
 	setPreview(item: IProduct) {
-
+		this.preview = item;
+		this.events.emit('preview:change', item);
 	}
 
 	isInBasket(item: IProduct) {
@@ -32,15 +36,21 @@ export class AppData {
 	}
 
 	addToBasket(item: IProduct) {
-	
+		this.basket.items.push(item.id);
+		this.basket.total += item.price;
+		this.events.emit('basket:change', this.basket);
     }
 
 	removeFromBasket(item: IProduct) {
-
+		this.basket.items = this.basket.items.filter(id => {item.id !== id});
+		this.basket.total -= item.price;
+		this.events.emit('basket:change', this.basket);
 	}
 
 	clearBasket() {
-
+		this.basket.items = [];
+		this.basket.total = 0;
+		this.events.emit('basket:change');
 	}
 
 	setPayment(method: PaymentMethod) {
@@ -48,18 +58,47 @@ export class AppData {
 	}
 
 	setOrderField(field: keyof TOrder, value: string) {
-
+		if (field === 'payment') {
+			this.setPayment(value as PaymentMethod);
+		}
+		else {
+			this.order[field] = value;
+		}
 	}
 
 	validateOrderForm() {
-
+		const errors: typeof this.formErrors = {};
+		if (!this.order.address) {
+			errors.address = "Укажи свой адрес!!!";
+		}
+		this.formErrors = errors;
+		this.events.emit('orderFormErrors:change', this.formErrors);
+		return Object.keys(errors).length === 0;
 	}
 
 	validateContactsForm() {
-
+		const errors: typeof this.formErrors = {};
+		if (!this.order.email){
+			errors.email = "Укажите Email пж";
+		} else if (!this.order.email.includes('@')) {
+			errors.email = 'Неправильно указан Email((';
+		}
+		if (!this.order.phone) {
+			errors.phone = 'Необходимо указать телефон';
+		} else if (!/^(\+7|8)[0-9]{10}$/.test(this.order.phone.replace(/[^0-9+]/g, ''))) {
+			errors.phone = 'А ничё тот факт что номер должен быть из цифр?';
+		}
+		this.formErrors = errors;
+		this.events.emit('contactsFormErrors:change', this.formErrors);
+		return Object.keys(errors).length === 0;
 	}
 
 	clearOrder() {
-
+		this.order = {
+			email: '',
+			phone: '',
+			address: '',
+			payment: 'card',
+		};
 	}
 }
